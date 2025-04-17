c3c3fa2e72876b38adda52f365fce6cd3c923fa2
anant-jain01 <jain.anant609@gmail.com>
2025-04-17 15:50:11 +0530

    modified:   10-fs-sync.js

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -2,3 +2,8 @@ const { readFileSync, writeFileSync } = require("fs");
 const first = readFileSync("./content/first.txt", "utf8");
 const second = readFileSync("./content/second.txt", "utf8");
 console.log(first, second);
+const third = writeFileSync("./content/result-sync.txt", "reasult@123");
+console.log(third);
+const read_third = readFileSync("./content/result-sync.txt", "utf8");
+console.log(read_third);
+//flag:a appends
