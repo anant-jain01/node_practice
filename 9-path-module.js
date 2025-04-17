ec2e200d800d26bc8eb0face0cb76554afd478e3
anant-jain01 <jain.anant609@gmail.com>
2025-04-17 15:17:42 +0530

    modified:   9-path-module.js

--- 9-path-module.js
+++ 9-path-module.js
@@ -1,3 +1,8 @@
-const path = requier("path");
+const path = require("path");
 console.log(path.sep);
-const 
+const filePath = path.join("/content", "subfolder", "test.txt");
+console.log(filePath);
+const base = path.basename(filePath);
+console.log(base);
+const absolue=path.resolve(__dirname,'content','subfolder','test.txt')
+console.log(absolue)
